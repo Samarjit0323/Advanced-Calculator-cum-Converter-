f0d58f7b157df48bf84565c29ec6ee5868d693e3
Samarjit0323 <samarjit19official@gmail.com>
2024-01-27 19:20:12 +0530

    Update 2

--- hex-dec.py
+++ hex-dec.py
@@ -1,9 +1,8 @@
 #Convert hexadecimal value to a decimal one
-char=["A","B","C","D","E","F"]
-charnum=[10,11,12,13,14,15]
+
 dec=0
-n=str(input("Enter hexadecimal value:"))
-lst=list(n)
+num=str(input("Enter hexadecimal value:"))
+lst=list(num)
 for a in range(len(lst)):
     if lst[a].isalpha()==False:
         y=1
@@ -15,8 +14,7 @@ for a in range(len(lst)):
         z=1
         for i in range(len(lst)-1-a):
             z=z*16
-        s=char.index(lst[a])
-        k=charnum[s]
-        m=k*z
+        s=chardict.get(lst[a])
+        m=s*z
         dec=dec+m
 print("Equivalent decimal number:",dec)
